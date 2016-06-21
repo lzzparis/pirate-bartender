ef186e392aee7bae4871ba9caa699af48dc99ece
Lizzie Paris <lzz.paris@gmail.com>
2016-06-20 19:16:06 -0700

    ui functional but not pretty

--- js/app.js
+++ js/app.js
@@ -27,16 +27,53 @@ var pantry = {
 var Bartender = function (name){
 	this.name = name;
 	this.questions = [];
+	this.questionNumber = 0;
 }
 
 Bartender.prototype.addQuestion = function(questionFlavor,questionText){
 	this.questions.push(new Question(questionFlavor, questionText));
 }
 
+Bartender.prototype.displayQuestion = function(){
+	var i = this.questionNumber;
+	var text = this.questions[i].text;
+
+	$(".bartender-form").html("");
+	//todo - just update the text, leave the input on the page
+	$(".bartender-form").html(text+"<input class=\"bartender-input\" type=\"text\" autofocus><input type=\"submit\">");
+	console.log(text);
+}
+
+Bartender.prototype.incrementQuestion = function(){
+	this.questionNumber +=1;
+}
+
+Bartender.prototype.recordResponse = function(resp){
+	var i = this.questionNumber;
+	var success = false;
+		if(resp === "y"){
+			this.questions[i].preference = true;
+			success = true;
+		}
+		else if(resp === "n"){
+			this.questions[i].preference = false;
+			success = true;
+		}
+		else alert("Please just respond \"y\" or \"n\" for now");
+	return success;
+}
+
+
+Bartender.prototype.createDrink = function(){
+	$(".bartender-form").html("");
+	$(".bartender-form").html("Here's your drink");
+}
+
+
 var Question = function(flavor, text){
 	this.flavor = flavor;
 	this.text = text;
-	this.preference = false;
+	this.preference = null;
 }
 
 var blackbeard = new Bartender("Blackbeard");
@@ -55,6 +92,34 @@ blackbeard.addQuestion("bitter","Are ye a lubber who likes it bitter?");
 blackbeard.addQuestion("sweet","Would ye like a bit of sweetness with yer poison?");
 blackbeard.addQuestion("fruity","Are ye one for a fruity finish?");
 
+//display the first question
+$("#start").click(function(){
+	blackbeard.displayQuestion();
+});
+
+
+//when submitting a response
+$(".bartender-form").submit(function(event){
+	//prevent the page from refreshing
+	event.preventDefault();
+	//get the response from the text box
+	var response = $(this).children(".bartender-input").val();
+	//record the response and store the return value
+	var success = blackbeard.recordResponse(response);
+	console.log(blackbeard.questionNumber+" "+success);
+	//if the record was successful ("y" or "n")
+	if (success){
+		//increase the question counter by 1
+		blackbeard.incrementQuestion();
+		//if we're not on the last question
+		if(blackbeard.questionNumber !== blackbeard.questions.length){
+			//display the next question
+			blackbeard.displayQuestion();
+		}
+		else blackbeard.createDrink();
+	}
+	//else do nothing
+});
 
 console.log(pantry);
 console.log(blackbeard);
